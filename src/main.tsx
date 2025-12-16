dfe5096dd35ace15394f684bbd034be78ba62135
Joshepshin <masin556@gmail.com>
2025-12-16 19:44:34 +0900

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,3 @@
-// DEBUG: Global Error Handler
-window.onerror = function (message, source, lineno, colno, error) {
-  alert('Global Error:\n' + message + '\nLine: ' + lineno + '\nCol: ' + colno + '\nSource: ' + source + '\nError: ' + error);
-};
-window.onunhandledrejection = function (event) {
-  alert('Unhandled Promise Rejection:\n' + event.reason);
-};
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
